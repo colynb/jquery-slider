c79a23d2229379f1c105df27640338a330bd5576
Colyn Brown <colyn.brown@gmail.com>
2013-01-27 20:28:15 -0700

    Added script/styles

---
+++ js/jquery-slider.js
@@ -0,0 +1,90 @@
+/* ==========================================================
+ * jquery-slider.js v0.1 Beta
+ * https://github.com/colynb/jquery-slider
+ * ==========================================================
+ * Copyright 2013, Colyn Brown.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================================================== */
+
+(function(){
+
+	$.widget( "cb.slider", {
+		_create: function() {
+			
+			self = this;
+
+			this.element
+				.wrap( "<div class='slider-container'></div>" );
+
+			this.element.attr('rel', this.options.slideDirection);
+
+			this._items = $('li', this.element);
+
+			this._items.each(function(i,item){
+				$item = $(item);
+				$item.addClass('slider-item')
+				if (i == 0) {
+					$item.addClass('active');
+				}
+			});
+			this._current = 0;
+			this._count = this._items.length;
+			this._run();
+		},
+
+		_setInterval: function(func, delay, context) {
+			return setInterval(function(){
+				func.call(context)
+			}, delay)
+		},
+
+
+		_uniqueId: function() {
+			var d = new Date;
+			return d.getTime();
+		},
+
+		_showSlide: function(i) {
+			this._current = i;
+			if (this.options.slideDirection == 'left') {
+				var opts = {left: -(this.options.width * i)};
+			} else if (this.options.slideDirection == 'right') {
+				var opts = {right: -(this.options.width * i)};
+			} else {
+				var opts = {top: -(this.options.height * i)};
+			}
+			this.element.animate(opts, this.options.duration);
+		},
+
+		_run: function() {
+			this._interval = this._setInterval(function(){
+				this._rotateSlides();
+			}, this.options.delay, this);
+		},
+
+		_rotateSlides: function() {
+			
+			var $curr = $(this._items[this._current]),
+				next = 0;
+
+			if (this._current == this._count - 1) {
+				next = 0;
+			} else {
+				next = this._current + 1;
+			}
+			this._showSlide(next);
+		}
+
+	});
+})();
